1cb90b37cb8b83b7fd48e107a257d779ed5d2928
Nicola R <nicola.rizzi89@hotmail.it>
2020-11-24 12:00:48 +0100

    Rebuild
    Everything works.

--- assets/campominato.js
+++ assets/campominato.js
@@ -6,31 +6,60 @@
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-
+//******************************************************** */
 // Faccio generare 16 numeri casuali tra 1 e 100
 
-var pcNumber= [];
-for (var i = 0; i < 16; i++){
-    pcNumber.push(Math.floor(Math.random() * (100 - 1) + 1));
+function random(min, max){
+    return Math.floor(Math.random() * (max-min +1) ) +min;
 }
-console.log(pcNumber); 
+var pcNumber = [];
+//******************************************************** */
+// QUESTO MI PERMETTE DI CONTROLLARE CHE NON CI SIANO DOPPIONI  ALL'INTERNO DELL'ARRAY, E DI GENERARE 16 NUMERI.
+while(pcNumber.length !== 16){
+    var number = random(1, 100);
+    if(! inArray(pcNumber, number)) {  // Se la condizione è falsa allora pusha il numero nell'array.
+        pcNumber.push(number);
+    }
+}
+console.log(pcNumber);
+//******************************************************** */
+// Ricercare un numero all'interno di un array
+function inArray (array, number){
+    var i = 0;
+    while ( i < array.length){
+        if (number === array[i]){     
+            return true;
+        }
+        i++;
+    }
+}
+//******************************************************** */
 
 // Chiedo all'utente di inserire un numero compreso tra 1 e 100, se il numero non è presente all'interno dell'array pcNumber, allora continuo a chiedere un numero fino un massimo di tot.
 
 
-var userNumber = prompt("Inserisci un numero compreso tra 1 e 100");
-var numberToCheck = [];
-var found = false;
-for (var j=0; j<pcNumber.length; j++) {
-    numberToCheck.push(Number(userNumber));
-    if(numberToCheck > 100){
-        alert("You have insered a wrong value");
-         break;
-     }else if (pcNumber[j]==numberToCheck){
-        found = true;
-        alert("You Win");
+var userNumber = [];
+var possibilità = 100 - 16;
+var numeroVietato = [];
+for (var i=0; i<=5; i++){
+    var numeroUtente = Number(prompt("Inserisci un numero da 1 a 100"));
+    if (inArray(pcNumber,numeroUtente )){
+        console.log("BOMBA");
         break;
-     } 
+    } else if(! inArray(userNumber, numeroUtente)){
+        numeroVietato.push(numeroUtente);
+    }
+    
+    while(inArray(userNumber, numeroUtente)){
+        alert("Hai gia usato questo numero!")
+        numeroUtente = Number(prompt("Inserisci un numero da 1 a 100"));
+    }
+    userNumber.push(numeroUtente);
+    
 }
 
-console.log(numberToCheck)
+
+
+console.log(userNumber);
+console.log(numeroVietato);
+console.log("Il tuo punteggio è " + (userNumber.length - numeroVietato.length));
