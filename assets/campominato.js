9cf804c29e8cdc4161dcaee6c18384c1c6450e7c
Nicola R <nicola.rizzi89@hotmail.it>
2020-11-24 12:46:46 +0100

    Bonus

--- assets/campominato.js
+++ assets/campominato.js
@@ -5,7 +5,33 @@
 // Se il numero è presente nella lista dei numeri generati, la partita termina, altrimenti si continua chiedendo all’utente un altro numero.
 // La partita termina quando il giocatore inserisce un numero “vietato” o raggiunge il numero massimo possibile di numeri consentiti.
 // Al termine della partita il software deve comunicare il punteggio, cioè il numero di volte che l’utente ha inserito un numero consentito.
+// BONUS: (da fare solo se funziona tutto il resto)
+// all’inizio il software richiede anche una difficoltà all’utente che cambia il range di numeri casuali:
+// con difficoltà 0 => tra 1 e 100
+// con difficoltà 1 =>  tra 1 e 80
+// con difficoltà 2 => tra 1 e 50
 
+//******************************************************** */
+var difficoltà = Number(prompt("Inserisci un numero da 0 a 2 per selezionare la difficoltà")); 
+switch (difficoltà) {
+    case 0:
+        difficoltà = 100;
+        alert("Easy");
+        break;
+    case 1:
+        difficoltà = 80;
+        alert("Medium");
+        break;
+    case 2:
+        difficoltà = 50;
+         alert("Hard");
+        break;
+    default :      
+        alert("Inserisci la dificoltà");
+        break;
+}
+
+console.log(difficoltà);
 //******************************************************** */
 // Faccio generare 16 numeri casuali tra 1 e 100
 
@@ -37,27 +63,32 @@ function inArray (array, number){
 
 // Chiedo all'utente di inserire un numero compreso tra 1 e 100, se il numero non è presente all'interno dell'array pcNumber, allora continuo a chiedere un numero fino un massimo di tot.
 
+// Se l'utente inserisce lo stesso numero 2 volte, lo inserisco all'interno di numeroVietato, e lo sottraggo al punteggio finale.
 
 var userNumber = [];
-var possibilità = 100 - 16;
 var numeroVietato = [];
-for (var i=0; i<=5; i++){
-    var numeroUtente = Number(prompt("Inserisci un numero da 1 a 100"));
-    if (inArray(pcNumber,numeroUtente )){
-        console.log("BOMBA");
-        break;
-    } else if(! inArray(userNumber, numeroUtente)){
-        numeroVietato.push(numeroUtente);
+
+
+
+function askNumber(userNmbrs){
+    for (var i=0; i<difficoltà; i++){
+        var numeroUtente = Number(prompt("Inserisci un numero da 1 a 100"));
+        if (inArray(pcNumber,numeroUtente )){
+            console.log("BOMBA");
+            break;
+        } else if(! inArray(userNumber, numeroUtente)){
+            numeroVietato.push(numeroUtente);
+        }
+        
+        while(inArray(userNumber, numeroUtente)){
+            alert("Hai gia usato questo numero!")
+            numeroUtente = Number(prompt("Inserisci un numero da 1 a 100"));
+        }
+        userNumber.push(numeroUtente);
     }
-    
-    while(inArray(userNumber, numeroUtente)){
-        alert("Hai gia usato questo numero!")
-        numeroUtente = Number(prompt("Inserisci un numero da 1 a 100"));
-    }
-    userNumber.push(numeroUtente);
-    
 }
 
+askNumber();
 
 
 console.log(userNumber);
